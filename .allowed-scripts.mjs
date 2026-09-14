e5c5c1597d815ac2ba7d536d7b3a675859d58b96
Igor <igor.ushkarev1@justice.gov.uk>
2026-09-14 10:45:05 +0100

    Fix allowed scripts

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -10,8 +10,6 @@ export default configureAllowedScripts({
     'node_modules/esbuild@0.28.2': 'ALLOW',
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
-    // Need by playwright for detecting file system changes during test runs
-    'node_modules/playwright/node_modules/fsevents@2.3.2': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
     'node_modules/unrs-resolver@1.12.2': 'ALLOW',
   },
